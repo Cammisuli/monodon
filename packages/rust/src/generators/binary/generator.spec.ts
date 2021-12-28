86d06007f79555f6fb0b8f79d81ff3d6aea78849
Jonathan Cammisuli <jon@cammisuli.ca>
2021-12-28 15:51:52 -0500

    update generator specs

--- packages/rust/src/generators/binary/generator.spec.ts
+++ packages/rust/src/generators/binary/generator.spec.ts
@@ -1,6 +1,6 @@
 import { createTreeWithEmptyWorkspace } from '@nrwl/devkit/testing';
 import { Tree, readProjectConfiguration } from '@nrwl/devkit';
-
+import TOML from '@ltd/j-toml';
 import generator from './generator';
 import { RustBinaryGeneratorSchema } from './schema';
 
@@ -16,5 +16,36 @@ describe('rust generator', () => {
     await generator(appTree, options);
     const config = readProjectConfiguration(appTree, 'test');
     expect(config).toBeDefined();
+  });
+
+  it('should create a Cargo.toml project', async () => {
+    await generator(appTree, { ...options });
+    const cargoToml = appTree.read('./apps/test/Cargo.toml')?.toString() ?? '';
+    expect(cargoToml).toBeDefined();
+    expect(TOML.parse(cargoToml)).toMatchInlineSnapshot(`
+      Object {
+        "dependencies": Object {},
+        "package": Object {
+          "edition": "2021",
+          "name": "test",
+          "version": "0.1.0",
+        },
+      }
+    `);
+  });
+
+  it('should create a project with a specified edition', async () => {
+    await generator(appTree, { ...options, edition: '2018' });
+    const cargoToml = appTree.read('./apps/test/Cargo.toml')?.toString() ?? '';
+    expect(TOML.parse(cargoToml)).toMatchInlineSnapshot(`
+      Object {
+        "dependencies": Object {},
+        "package": Object {
+          "edition": "2018",
+          "name": "test",
+          "version": "0.1.0",
+        },
+      }
+    `);
   });
 });
