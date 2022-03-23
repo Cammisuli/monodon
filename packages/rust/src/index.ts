390cab8f427853e62103a91cdad33c7b8514c85a
Jonathan Cammisuli <jon@cammisuli.ca>
2022-03-22 22:42:38 -0400

    add nx plugin type to export and revert to 13.9.x

--- packages/rust/src/index.ts
+++ packages/rust/src/index.ts
@@ -1,1 +1,12 @@
-export { processProjectGraph } from './graph';
+import { NxPlugin } from '@nrwl/devkit';
+import { processProjectGraph } from './graph';
+
+const nxPlugin: NxPlugin = {
+  name: '@monodon/rust',
+  processProjectGraph,
+  projectFilePatterns: ['Cargo.toml'],
+  // TODO(cammisuli): add support for registering targets
+  // registerProjectTargets,
+};
+
+export = nxPlugin;
