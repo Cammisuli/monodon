4b174ec3fb9dd04c7f2612d0cc8feec37a3b6c59
Jonathan Cammisuli <jon@cammisuli.ca>
2022-03-31 01:55:51 +0000

    scaffold test for build executor

--- packages/rust/src/executors/build/executor.spec.ts
+++ packages/rust/src/executors/build/executor.spec.ts
@@ -1,11 +1,17 @@
+import * as cargoUtils from '../../utils/cargo';
+jest.mock('../../utils/cargo', (): Partial<typeof cargoUtils> => {
+  return { runCargoSync: jest.fn(() => ({ output: 'output', success: true })) };
+});
+
 import { BuildExecutorSchema } from './schema';
+import {} from '@nrwl/devkit/testing';
 import executor from './executor';
 
 const options: BuildExecutorSchema = {};
 
 describe('Build Executor', () => {
   it('can run', async () => {
-    const output = await executor(options);
+    const output = await executor(options, {} as any);
     expect(output.success).toBe(true);
   });
 });
