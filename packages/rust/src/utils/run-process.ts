a05327892805d5944dcdc493ec7d6ee6fbc23ecf
Jonathan Cammisuli <jon@cammisuli.ca>
2022-12-08 09:32:37 -0500

    fix(rust): use execSync

--- packages/rust/src/utils/run-process.ts
+++ packages/rust/src/utils/run-process.ts
@@ -1,39 +1,18 @@
-import { ChildProcess, spawn } from 'child_process';
-
-export let childProcess: ChildProcess | null;
+import { execSync } from 'child_process';
 
 export function runProcess(
   processCmd: string,
   ...args: string[]
 ): { success: boolean } | PromiseLike<{ success: boolean }> {
-  return new Promise((resolve, reject) => {
-    childProcess = spawn(processCmd, [...args, '--color', 'always'], {
+  return new Promise((resolve) => {
+    execSync(processCmd + ' ' + args.join(' ') + ' --color always', {
       cwd: process.cwd(),
       env: {
         ...process.env,
         RUSTC_WRAPPER: '',
       },
-      stdio: ['pipe'],
+      stdio: ['inherit', 'inherit', 'inherit'],
     });
-
-    // cargo outputs to stderr instead of stdout. Redirect to stdout.
-    childProcess.stderr?.pipe(process.stdout);
-
-    // Ensure the child process is killed when the parent exits
-    process.on('exit', () => childProcess?.kill());
-    process.on('SIGTERM', () => childProcess?.kill());
-
-    childProcess.on('error', (err) => {
-      reject({ success: false });
-    });
-
-    childProcess.on('exit', (code) => {
-      childProcess = null;
-      if (code === 0) {
-        resolve({ success: true });
-      } else {
-        reject({ success: false });
-      }
-    });
+    resolve({ success: true });
   });
 }
