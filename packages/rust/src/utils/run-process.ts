cb70d69e3dcd63fa3c5510f89528fd3c5821e14e
Jonathan Cammisuli <jon@cammisuli.ca>
2023-01-05 11:45:46 -0500

    feat(rust): add cargo config

--- packages/rust/src/utils/run-process.ts
+++ packages/rust/src/utils/run-process.ts
@@ -1,15 +1,19 @@
 import { execSync } from 'child_process';
+import { joinPathFragments, workspaceRoot } from '@nrwl/devkit';
 
 export function runProcess(
   processCmd: string,
   ...args: string[]
 ): { success: boolean } | PromiseLike<{ success: boolean }> {
+  const targetDir = joinPathFragments(workspaceRoot, 'dist', 'cargo');
   return new Promise((resolve) => {
     execSync(processCmd + ' ' + args.join(' '), {
       cwd: process.cwd(),
       env: {
         ...process.env,
         RUSTC_WRAPPER: '',
+        CARGO_TARGET_DIR: targetDir,
+        CARGO_BUILD_TARGET_DIR: targetDir,
       },
       stdio: ['inherit', 'inherit', 'inherit'],
     });
